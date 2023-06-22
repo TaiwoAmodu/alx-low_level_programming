b2c0a9cc3614821a60436dd6d6c0c7f449c10d85
TaiwoAmodu <amodutaiwo22@gmail.com>
2023-06-21 20:16:50 -0700

    assada

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * Return: 1 and prints '+' if n is greater than zero
+ * 0 and prints '0' if n is zero
+ * -1 and prints '-' if n is less than zero
  */
 int print_sign(int n)
 {
@@ -25,6 +25,5 @@ int print_sign(int n)
 		return (-1);
 	}
 
-	return (0); /* Optional return statement in case n doesn't fit
-               any condition */
+	return (0); /* Add this return statement */
 }
