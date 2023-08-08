b0fd130e23fe231d842d30d60b99160035bc48db
TaiwoAmodu <amodutaiwo22@gmail.com>
2023-08-08 04:06:16 -0700

    0-read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,14 +13,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	ssize_t fd;
 	ssize_t w;
-	ssize_t z;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	z = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, z;
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
